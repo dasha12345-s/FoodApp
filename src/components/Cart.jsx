ac0e03d5756f01e12764b72853daf2bb049b4515
Darya <shostak19@icloud.com>
2024-05-12 13:07:08 -0400

    Working on Cart Items

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,6 +4,7 @@ import CartContext from "../store/CartContext.jsx";
 import { currencyFormatter } from "../util/formatting";
 import Button from "./UI/Button.jsx";
 import UserProgresContext from "../store/UserProgresContext.jsx";
+import CartItem from "./UI/CartItem.jsx";
 
 export default function Cart(){
 
@@ -21,15 +22,18 @@ function handleCloseCart(){
       <h2>Your cart</h2>
       <ul>
         {cartCtx.items.map(item => (
-        <li key={item.id}>
-          {item.name} - {item.quantity}
-        </li>
+        <CartItem 
+        key={item.id} 
+        item={item}
+        onIncrease={() => cartCtx.addItem(item)}
+        onDecrease={() => cartCtx.removeItem(item.id)}
+        />
         ))}
       </ul>
       <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
       <p>
         <Button textOnly onClick={handleCloseCart}>Close</Button>
-        <Button>Go to Checkout</Button>
+        <Button onClick={handleCloseCart}>Go to Checkout</Button>
       </p>
     </Modal>
   )
