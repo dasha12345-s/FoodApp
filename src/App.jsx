71ef16f3ce23585a1457822ee446426f205e4753
Darya <shostak19@icloud.com>
2024-05-12 13:07:08 -0400

    Adding a Custom Input Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Cart from "./components/Cart.jsx";
 import Header from "./components/Header.jsx";
 import Meals from './components/Meals.jsx';
+import Checkout from "./components/UI/Checkout.jsx";
 import { CartContextProvider } from "./store/CartContext.jsx";
 import { UserProgresContextProvider } from "./store/UserProgresContext.jsx";
 
@@ -11,6 +12,7 @@ function App() {
       <Header/>
       <Meals/>
       <Cart/>
+      <Checkout/>
     `</CartContextProvider>
    </UserProgresContextProvider> 
   );
--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -11,14 +11,18 @@ export default function Cart(){
  const cartCtx = useContext(CartContext)
  const userProgressCtx = useContext(UserProgresContext);
 
- const cartTotal = cartCtx.items.reduce((totalPrice, item) => totalPrice + item.quantity * item.price , 0)
+ const cartTotal = cartCtx.items.reduce((totalPrice, item) => totalPrice + item.quantity * item.price , 0);
 
 function handleCloseCart(){
   userProgressCtx.hideCart();
 }
 
+function handleGoToCheckout(){
+  userProgressCtx.showCheckout();
+};
+
   return(
-    <Modal className="cart" open={userProgressCtx.process === 'cart'}>
+    <Modal className="cart" open={userProgressCtx.process === 'cart'} onClose={userProgressCtx.process === 'cart' ? handleCloseCart : null}>
       <h2>Your cart</h2>
       <ul>
         {cartCtx.items.map(item => (
@@ -31,9 +35,9 @@ function handleCloseCart(){
         ))}
       </ul>
       <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
-      <p>
+      <p className="modal-actions">
         <Button textOnly onClick={handleCloseCart}>Close</Button>
-        <Button onClick={handleCloseCart}>Go to Checkout</Button>
+        {cartCtx.items.length > 0 && (<Button onClick={handleGoToCheckout}>Go to Checkout</Button>)}
       </p>
     </Modal>
   )
